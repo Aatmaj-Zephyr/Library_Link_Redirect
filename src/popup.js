6e30f7ea0f3c3403fd5e972e5047a4f3075d37ce
Aatmaj <Aatmaj.mhatre@gmail.com>
2024-03-29 14:44:49 +0530

    update popup.js Fixed bug

--- src/popup.js
+++ src/popup.js
@@ -1,13 +1,15 @@
+/* The code snippet you provided is using JavaScript to listen for the 'DOMContentLoaded' event on the
+window object. When this event is triggered (indicating that the initial HTML document has been
+completely loaded and parsed), an asynchronous function is executed. */
 window.addEventListener('DOMContentLoaded', async () => {
     const currentUrl = chrome.tabs
       .query({ active: true, currentWindow: true })
       .then((tabs) => tabs[0].url);
 
-    //if ieee
     const newUrl = await currentUrl.then((url) => redirectURL(url));
 
     chrome.tabs.update({ url: newUrl });
-    window.close(); // Close the popup immediately
+    window.close(); // Close the popup immediately after the URL has been changed.
 });
 
 function redirectURL(url) {
@@ -16,7 +18,7 @@ function redirectURL(url) {
           "https://ieeexplore.ieee.org/",
           "https://ieeexplore-ieee-org.library.somaiya.edu/",
         );
-      } else if (url.includes("dl.acm.org")) {
+  } else if (url.includes("dl.acm.org")) {
         url = url.replace(
           "https://dl.acm.org/",
           "https://dl-acm-org.library.somaiya.edu/",
@@ -31,11 +33,10 @@ function redirectURL(url) {
   else if (url.includes("sciencedirect.com")) {
         url = url.replace(
           "https://www.sciencedirect.com/",
-          "https://www-sciencedirect-com.library.somaiya.edu",
+          "https://www-sciencedirect-com.library.somaiya.edu/",
         );
   }
   
-
-      return url; // Return the modified URL
+  return url; // Return the modified URL
  
 }
